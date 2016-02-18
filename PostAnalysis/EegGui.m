7e4f0b8b429fea5a7dd3e52ba4bf9ee38d02994f
asteroidDavis <natedavis174@gmail.com>
2016-02-18 09:38:52 -0500

    Task adding functionality.

--- PostAnalysis/EegGui.m
+++ PostAnalysis/EegGui.m
@@ -18,6 +18,10 @@ classdef EegGui < handle
             tasksPanel
                 tasksSelectionText
                 taskSelection
+                tasks
+                defaultTaskMessage
+                addTask
+                removeTask
                 electrodesSelectionText
                 electrodeSelection
                 stepsPerPlotText
@@ -26,15 +30,40 @@ classdef EegGui < handle
     end
     
     methods(Access = private)
-        %When the figure is resized
-        %Then resize the panels within the figure
-        function success =  resizeUI(this)
-            %gets the current figure position array
-            outerPosition = this.eegFigHandle.OuterPosition;
-            figWidth = outerPosition(3);
-            figHeight = outerPosition(4);
-            
-            success = 1;
+        
+        %When the user clicks the + next to 'Tasks'
+        function success = toggleTaskSelection(this)
+            %And the task selection field is a dropdown
+            if(strcmp(get(this.taskSelection, 'Style'), 'popupmenu') == 1)
+                %Then the task selection field becomes editable text
+                this.taskSelection = uicontrol(this.tasksPanel, 'Style',...
+                    'edit', 'FontSize', 10, 'Units', 'normalized',...
+                    'Position', [0 2/3 1 1/6]);
+                disp('Toggled');
+                success = 1;
+            %Or the task selection field is editable text
+            elseif(strcmp(get(this.taskSelection, 'Style'), 'edit') == 1)
+                this.tasks{end+1} = get(this.taskSelection, 'String');
+                %And the user added tasks
+                if(size(this.tasks) >= 1)
+                    %display a task selector with the added tasks
+                    this.taskSelection = uicontrol(this.tasksPanel, 'Style',...
+                        'popupmenu', 'FontSize', 10, 'Units', 'normalized',...
+                        'Position', [0 2/3 1 1/6], 'String', this.tasks);
+                else
+                    %display the default task selector
+                    this.taskSelection = uicontrol(this.tasksPanel, 'Style',...
+                        'popupmenu', 'FontSize', 10, 'Units', 'normalized',...
+                        'Position', [0 2/3 1 1/6],...
+                        'String', {this.defaultTaskMessage});
+                end
+                success = 1;
+            else
+                warning(strcat('Task Selector: taskSelection is type: ',...
+                    get(this.taskSelection, 'Style'), '. \nResetting to ',...
+                    'popupmenu.'));
+                success = 0;
+            end
         end
         
     end
@@ -44,8 +73,7 @@ classdef EegGui < handle
         function this = EegGui()
             %extends figure
             this.eegFigHandle = figure('Visible', 'off', 'Tag', 'fig',...
-                'SizeChangedFcn', @(src, event) resizeUI(this), 'Units', ...
-                'characters');
+                'Units', 'characters');
             
             %puts a menue panel at the left side of the gui
             this.menuPanel = uipanel('Title', 'Controls', 'FontSize', 11,...
@@ -74,13 +102,23 @@ classdef EegGui < handle
             this.tasksPanel = uipanel(this.menuPanel, 'Title', 'Task Info',...
                 'FontSize', 11, 'BackgroundColor', 'white', 'Position',...
                 [0 2/5 1 2/5]);
+            %labels the task selection area
             this.tasksSelectionText = uicontrol(this.tasksPanel, 'Style', 'text',...
                 'String', 'Tasks', 'FontSize', 10, 'Units', 'normalized',... 
                 'BackgroundColor', 'white', 'Position', [0 5/6 1/2 1/6]);
+            %provides a menu to add tasks
+            this.addTask = uicontrol(this.tasksPanel, 'Style', 'pushbutton',...
+                'Fontsize', 10, 'BackgroundColor', 'white', 'String', '+',...
+                'Units', 'normalized', 'Position', [1/2 5/6 1/4 1/6],...
+                'Callback', @(src, event) toggleTaskSelection(this));
+            %provides a drop down or editable text for adding task labels
+            this.defaultTaskMessage = 'Press + to add tasks';
+            this.tasks = {};
             this.taskSelection = uicontrol(this.tasksPanel, 'Style',...
                 'popupmenu', 'FontSize', 10, 'Units', 'normalized',...
                 'Position', [0 2/3 1 1/6], 'String',...
-                {'Press + to add task names'});
+                {this.defaultTaskMessage});
+
             
             %a plots panel at the right side of the gui
             this.plotsPanel = uipanel('Title', 'Plots', 'FontSize', 10, ...
