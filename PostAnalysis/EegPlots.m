ddf51f7396525886581c974a7e8b3fbd5aa91e7c
asteroidDavis <natedavis174@gmail.com>
2016-02-18 17:57:40 -0500

    Fixed errors in EegPlots and finished constructor.

--- PostAnalysis/EegPlots.m
+++ PostAnalysis/EegPlots.m
@@ -4,55 +4,62 @@ classdef EegPlots
     
     properties(Access = private)
         axis
-%             axis.x
-%                 axis.x.label
-%                 axis.x.lim
-%                 axis.x.domain
-%             axis.y
-%                 axis.y.label
-%                 axis.y.lim
-%                 axis.y.domain
         electrodes
         tasks
     end
     
     methods(Access = public)
-        function this = EegPlots(xaxis, yaxis)
-            this;
-            setAxis(this, 'x', xaxis, 'y', yaxis);
-        end
         
-        function setAxis(this, varargin)
-            %allows case insentive parameters and ignore unknown parameters
-            parser = inputParser('CaseSensitive', 0, 'KeepUnmatched', 1);
+        %constructs the EegPlots
+        function this = EegPlots(varargin)
+             %allows case insentive parameters and ignore unknown parameters
+            parser = inputParser;
+            parser.StructExpand = 0;
             
             %creates default axis when none are expressed
             defaultXAxis = struct('label', 'x-axis', 'lim', [0 60],...
-                'domain', {'frequency', 'time'});
+                'domain', {{'frequency', 'time'}});
             defaultYAxis = struct('label', 'y-axis', 'lim', [-1 1],...
-                'domain', {'normalized data'});
+                'domain', {{'normalized data', 'log power'}});
             
-            %allows named labeled unorder parameters
-            addOptional(parser, 'x', defaultXAxis, @validateAxis);
-            addOptional(parser, 'y', defaultYAxis, @validateAxis);
+            %defines and orders the parameters
+            %electrodes must be the first parameter to EegPlots
+            addRequired(parser, 'electrodes', @iscellstr);
+            %tasks must be the second parameter to EegPlots
+            addRequired(parser, 'tasks', @iscellstr);
+            %the axis can be in any order
+            addParameter(parser, 'x', defaultXAxis, @validateAxis);
+            addParameter(parser, 'y', defaultYAxis, @validateAxis);
             
+            %parses and passes the arguments to the EegPlots object
             parse(parser, varargin{:});
             this.axis.x = parser.Results.x;
-            this.axis.y = parser.Ressults.y;
+            this.axis.y = parser.Results.y;
+            this.electrodes = parser.Results.electrodes;
+            this.tasks = parser.Results.tasks;
         end
-        function axis = getAxis(this)
-           axis = this.axis;
+        
+        %validates and sets the axis object
+        function setAxis(this, xAxis, yAxis)
+            if(this.validateAxis(xAxis) && this.validateAxis(yAxis))
+                this.axis.x = xAxis;
+                this.axis.y = yAxis;
+            else
+                warning('xAxis, yAxis do not pass validation for axis.setAxis');
+            end
+        end
+        function axes = getAxis(this)
+           axes = this.axis;
         end
     end
     
     methods(Access = private)
+        
         %Given an axis variable
-        function isAxis = validateAxis(testee)
+        function isAxis = validateAxis(this, testee)
             %Is the vairable a struct with a label, lim, and domain
-            isAxis = isstruct(testee) &&  exist(testee.label, 'var') &&...
-                exist(testee.lim, 'var') && exist(testee.domain, 'var') &&...
-                ischar(testee.label) && isnumeric(testee.lim) &&...
-                iscellstr(testee.domain);
+            isAxis = isstruct(testee) && ischar(testee.label) &&...
+                isnumeric(testee.lim) && iscellstr(testee.domain);
         end
     end
 end
