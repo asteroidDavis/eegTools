73705f5eef434e6f6936f20c8628ac403867e0a9
asteroidDavis <natedavis174@gmail.com>
2016-03-31 15:19:42 -0400

    Histograms and experiments

--- labs/p300Lab/analysis.m
+++ labs/p300Lab/analysis.m
@@ -11,10 +11,14 @@ channels = {'time', 'P3', 'Pz', 'P4', 'CPz', 'PO7', 'POz', 'PO8','Oz',...
     'sampleNo', 'trigger', 'choice', 'trialNo'};
 dataChannels = 2:9;
 
-%load the first trial
-exp1RawData = load('Nata_P300_Run2_2016_2_25_14_29_25.mat');
-%load the second trial with modified characteristics
-exp2RawData = load('Nata_P300_Run3_2016_2_25_14_52_49.mat');
+%BAD DATA
+% exp1RawData = load('Nata_P300_Run2_2016_2_25_14_29_25.mat');
+% exp2RawData = load('Nata_P300_Run3_2016_2_25_14_52_49.mat');
+
+% %load the first trial
+exp1RawData = load('Genius_P300_Run1_2016_3_3_13_35_31.mat');
+% %load the second trial with modified characteristics
+exp2RawData = load('Genius_P300_Run2_2016_3_3_14_34_45.mat');
 
 dataSrc = str2double(inputdlg('Experiment 1 or 2?'));
 if(dataSrc == 1)
@@ -59,7 +63,9 @@ end
 
 %%
 %filters the data with a 1-30Hz buttersworth bandpass filter
-[bDesign, aDesign] = butter(2, [1 30]/facq, 'bandpass');
+filteredData = zeros(length(dataChannels), length(rawData(dataChannels(1),:)));
+
+[bDesign, aDesign] = butter(3, [0.2 15]/(facq/2), 'bandpass');
 for electrodeIndex = 1:length(dataChannels)
     filteredData(electrodeIndex, :) = filtfilt(bDesign, aDesign, rawData(dataChannels(electrodeIndex), :));
 end
@@ -67,22 +73,27 @@ end
 %%
 %Extract windows of all choices and non-choices
 
+%the min index where a epoch can take 250ms before
+minStartIndex = round(0.25*facq);
+%the max index where a epoch can take 500ms after
+maxEndIndex = length(rawData(channel, :)) - 0.5*facq;
+
 %Extract windows containing 250ms before and 500ms after each choice
 choiceWindows = zeros(length(dataChannels),length(choiceIndex),...
     round(0.75*facq));
 for channel = 1:length(dataChannels)
     choiceWindow = 0;
-    for electrodeIndex = choiceIndex
+    for i = choiceIndex
         %if a full window can be extracted
-        %if(i > 300 && i < length(rawData(1,:))-1000)
+        if(i > minStartIndex && i < maxEndIndex)
             choiceWindow = choiceWindow + 1;
             choiceWindows(channel, choiceWindow, 1:round(0.75*facq)) = filteredData(...
                 channel, round(choiceIndex(choiceWindow)-0.25*facq)...
                 :choiceIndex(choiceWindow)+0.5*facq);
-        %else
-           % warning(strcat('Skipping flash because there is ',...
-           %     'not a enough data ahead or behind the flash'));
-        %end
+        else
+            warning(strcat('Skipping flash because there is ',...
+                'not a enough data ahead or behind the flash'));
+        end
     end
 end
 
@@ -91,11 +102,16 @@ nChoiceWindows = zeros(length(dataChannels), length(nChoiceIndex),...
     round(0.75*facq));
 for channel = 1:length(dataChannels)
     nChoiceWindow = 0;
-    for electrodeIndex = nChoiceIndex
-        nChoiceWindow = nChoiceWindow+1;
-        nChoiceWindows(channel, nChoiceWindow, 1:round(0.75*facq)) = ...
-            filteredData(channel, round(nChoiceIndex(nChoiceWindow)...
-            -0.25*facq):nChoiceIndex(nChoiceWindow)+0.5*facq);
+    for i = nChoiceIndex
+        if(i > minStartIndex && i < maxEndIndex)
+            nChoiceWindow = nChoiceWindow+1;
+            nChoiceWindows(channel, nChoiceWindow, 1:round(0.75*facq)) = ...
+                filteredData(channel, round(nChoiceIndex(nChoiceWindow)...
+                -0.25*facq):nChoiceIndex(nChoiceWindow)+0.5*facq);
+        else
+            warning(strcat('Skipping flash because there is ',...
+                'not a enough data ahead or behind the flash'));
+        end
     end
 end
 
@@ -112,7 +128,6 @@ for electrodeIndex = 1:length(choiceWindows(:,1,1))
     avgChoice(electrodeIndex, :) = meanChoice(:);
     avgNChoice(electrodeIndex, :) = meanNChoice(:);
 end
-
 %%
 %plot the avg choice vs the average not choice 
 
@@ -128,7 +143,30 @@ for electrodeIndex = 1:length(choiceWindows(:,1,1))
     hold off;
 end
 
+%%
+%plots a normalized historgram of choice versus not choice
 
+cHistogram = cell(length(dataChannels));
+ncHistogram = cell(length(dataChannels));
+
+figure;
+for electrodeIndex = 1:length(choiceWindows(:, 1, 1))
+    subplot(round(length(cHistogram(:,1,1))/2), 2, electrodeIndex);
+    cHistogram{electrodeIndex} = histogram(avgChoice(electrodeIndex, :),...
+        'NumBins', 25, 'Normalization', 'probability');
+    hold on;
+    ncHistogram{electrodeIndex} = histogram(avgNChoice(electrodeIndex, :),...
+        'NumBins', 25, 'Normalization', 'probability');
+    title([channels{electrodeIndex+1} ' choice vs not choice']);
+    legend('Choice', 'Not Choice')
+    hold off;
+end
+
+%%
+%creates an average histogram of all epochs and channels for choice vs
+%non-choice
+
+%finds the mean of all 
 
 
 
